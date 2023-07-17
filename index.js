899e85f998885004470868d600abd09c6fa86656
Duca4 <139258477+Duca4@users.noreply.github.com>
2023-07-17 14:49:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var calculateButton = document.getElementById('calculateButton');
+  calculateButton.addEventListener('click', calculateTotal);
+});
 
+function calculateTotal() {
+  var quantityInput = document.getElementById('quantity');
+  var totalInput = document.getElementById('total');
+  
+  var quantity = parseInt(quantityInput.value);
+  var pricePerItem = 5;
+  var total = quantity * pricePerItem;
+  
+  totalInput.value = total;
+}
