37c04dd99ec91ea0a5461b1e94452127d63c95a5
Duca4 <139258477+Duca4@users.noreply.github.com>
2023-07-17 12:28:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-// Get references to the quantity input, calculate button, and result div
+
 const quantityInput = document.getElementById('quantityInput');
 const calculateButton = document.getElementById('calculateButton');
 const totalResult = document.getElementById('totalResult');
-
-// Add an event listener to the calculate button
 calculateButton.addEventListener('click', calculateTotal);
-
-// Function to calculate the total
 function calculateTotal() {
-  // Retrieve the quantity from the input
   const quantity = parseInt(quantityInput.value);
-
-  // Calculate the total
   const pricePerCandle = 5;
   const total = quantity * pricePerCandle;
 
-  // Display the total in the result div
   totalResult.textContent = 'Total: $' + total.toFixed(2);
 }
